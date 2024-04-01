6181fedc930d1d011195928c0625a268e7dacf94
jainil15 <79244804+jainil15@users.noreply.github.com>
2024-04-01 17:55:52 +0530

    Update page.tsx

--- app/main/managers/page.tsx
+++ app/main/managers/page.tsx
@@ -11,7 +11,7 @@ const ManagersPage = async () => {
     return (  
         <div className="p-5 h-full w-full">
             <EmptyState 
-                title="Staging Work underway Edited by Jainil"
+                title="Staging Work underway Edited by Jainil Staging Final"
             />
         </div>
     );
